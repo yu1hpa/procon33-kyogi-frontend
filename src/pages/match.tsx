0583a31b5b7c9741bf4174b49ec1fd987a886e00
yu1hpa <yu.1hpa@gmail.com>
2022-10-06 02:44:25 +0900

    feat(match): Add a page for match

---
+++ src/pages/match.tsx
@@ -0,0 +1,67 @@
+import styles from "./match.module.scss";
+
+export type Match = {
+  problems: number;
+  bonus_factor: number[];
+  penalty: number;
+};
+
+export const getStaticProps = async () => {
+  if (typeof process.env.PROCON_TOKEN === "undefined") {
+    return;
+  }
+
+  const res = await fetch(`${process.env.HOST}/match`, {
+    method: "get",
+    headers: {
+      "Context-Type": "application/json",
+      "procon-token": process.env.PROCON_TOKEN,
+    },
+  });
+  const match: Match = await res.json();
+  return { props: match };
+};
+
+const Match = (match: Match) => {
+  return (
+    <div className={styles.wrapper}>
+      {typeof match === "undefined" ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          <div className={styles.wrapper__table}>
+            <table className={styles.match_table}>
+              <thead>
+                <tr>
+                  <th colSpan={2}>試合の情報</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>問題数</td>
+                  <td>{match.problems}</td>
+                </tr>
+                <tr>
+                  <td>ボーナス係数</td>
+                  <td>{match.penalty}</td>
+                </tr>
+                <tr>
+                  <td>変更札に適用される係数</td>
+                  <td>
+                    [
+                    {match.bonus_factor.map((v) => {
+                      return [<span key={v.toString()}>{v}</span>];
+                    })}
+                    ]
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Match;
