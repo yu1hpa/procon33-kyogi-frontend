7170cc8a9ea7af56ea026ecf69b65966ae2a454f
yu1hpa <yu.1hpa@gmail.com>
2022-10-12 02:55:09 +0900

    feat: Add Top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+import styles from "./index.module.scss";
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>長野高専 Procon33 競技部門</title>
+        <title>高専プロコン 第33回群馬大会 競技部門</title>
         <meta name="description" content="Procon33 Kyogi" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>長野高専 Procon33 競技部門</h1>
+        <div className={styles.wrapper}>
+          <div className={styles.wrapper__main}>
+            <div className={styles.main}>
+              <h1 className={styles.main__h1}>第33回 高専プロコン 競技部門</h1>
+              <Link href="https://www.procon.gr.jp/?page_id=78119/">
+                <a className={styles.main__link}>群馬大会(2022)</a>
+              </Link>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
