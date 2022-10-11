bda2467c177f45730fb9d60b36467137c639e2a2
yu1hpa <yu.1hpa@gmail.com>
2022-10-12 00:22:17 +0900

    chore: Add a answers api

---
+++ src/pages/api/answers.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { AnswerResponse } from "../../types";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  let answers: AnswerResponse | undefined;
+  if (req.method === "GET") {
+    res.status(200).json({
+      answers: answers,
+    });
+  }
+
+  if (req.method === "POST") {
+    res.status(200).json({
+      problem_id: req.body.problem_id,
+      answers: req.body.answers,
+      accepted_at: Date.now(),
+    });
+  }
+}
