a329fff92466dac980da60fa095b99fa916fdfea
yu1hpa <yu.1hpa@gmail.com>
2022-10-13 20:08:06 +0900

    feat: Add an error message

--- src/commons.ts
+++ src/commons.ts
@@ -1,5 +1,21 @@
 type Header = {
   [key: string]: string;
 };
-export const fetcher = (url: string, header: Header) =>
-  fetch(url, header).then((res) => res.json());
+
+// matchとproblemのみに対するリクエスト
+export const fetcher = async (url: string, header: Header) => {
+  const res = await fetch(url, header);
+  if (!res.ok) {
+    let error = new Error("");
+    console.log(res);
+    switch (res.status) {
+      case 400:
+        error.message = "問題の回答時間外の可能性があります";
+        throw error;
+      default:
+        error.message = "不明なエラー";
+        throw error;
+    }
+  }
+  return res.json();
+};
